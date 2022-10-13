73b52aff55c1a60f373ff4f5d2e4d58f7175700c
Jaerker <johanjakberger@gmail.com>
2022-10-13 22:06:13 +0200

    test

--- server.js
+++ server.js
@@ -21,7 +21,10 @@ mongoose.connect(process.env.DB_URL,
 //Middlewares
 app.use(express.json());
 app.use(cors({
-    origin: 'https://blog-app-production-a708.up.railway.app'
+    origin: [
+        'https://blog-app-production-a708.up.railway.app',
+        'http://localhost:3000'    
+    ]
 }));
 
 
